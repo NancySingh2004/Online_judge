1acadac77a501b83fcff1298a24ede0d18afb947
NancySingh2004 <155474342+NancySingh2004@users.noreply.github.com>
2026-08-11 17:50:23 +0530

    updated

--- client/src/components/SolveProblem.jsx
+++ client/src/components/SolveProblem.jsx
@@ -1,4 +1,4 @@
-// SolveProblem.jsx
+
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -18,7 +18,7 @@ function SolveProblem() {
           <h2 className="text-xl font-bold">{problem.title}</h2>
           <p className="text-gray-700 mb-4">{problem.description}</p>
           <textarea className="w-full h-64 p-2 border" placeholder="Write your code here..."></textarea>
-          <button className="btn btn-success mt-2">🚀 Submit Code</button>
+          <button className="btn btn-success mt-2"> Submit Code</button>
         </>
       ) : (
         <p>Loading problem...</p>
